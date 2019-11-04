f8bbb2a8d63c6bc89b9a6ae00e7b1d9cb144459c
tanw.89 <tanw.89@gmail.com>
2019-11-05 01:38:56 +0800

    .

---
+++ stopvid.js
@@ -0,0 +1,20 @@
+var stopButton = document.getElementById('close-button-id1');
+stopButton.onclick = function() {
+var video = $("#youtube-player-id1",).attr("src");
+$("#youtube-player-id1").attr("src","");
+$("#youtube-player-id1").attr("src",video);
+}; 
+
+var stopButton2 = document.getElementById('close-button-id2');
+stopButton2.onclick = function() {
+var video = $("#youtube-player-id2",).attr("src");
+$("#youtube-player-id2").attr("src","");
+$("#youtube-player-id2").attr("src",video);
+}; 
+
+var stopButton3 = document.getElementById('close-button-id3');
+stopButton3.onclick = function() {
+var video = $("#youtube-player-id3",).attr("src");
+$("#youtube-player-id3").attr("src","");
+$("#youtube-player-id3").attr("src",video);
+};
